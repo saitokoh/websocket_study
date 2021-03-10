1491f47ac2bcb2e6a77d76c342745084d34bb723
saito.kohei <saito.kohei@raccoon.ne.jp>
2021-03-10 11:03:33 +0900

    答えinit

--- chat.js
+++ chat.js
@@ -2,10 +2,11 @@ new Vue({
   el: '#app',
   data: {
     name: "",
-    isLogin: false,
+    isLogined: false,
     messages: [],
     writingMessage: "",
     errorMessage: {name: "", writingMessage: ""},
+    ws: null
   },
   methods: {
     sendMessage() {
@@ -14,12 +15,13 @@ new Vue({
         this.errorMessage.writingMessage = "空白は送信できません"
         return
       }
-      this.messages.push(this.writingMessage);
-      this.writingMessage = "";
-      this.$nextTick().then(() => {
-        const displayMessagesEl = this.$refs.displayMessages
-        displayMessagesEl.scrollTop = displayMessagesEl.scrollHeight - displayMessagesEl.clientHeight;
+      this.send({
+        type: "send",
+        name: this.name,
+        text: this.writingMessage
       })
+      this.writingMessage = "";
+      
     },
     login() {
       this.errorMessage.name = ""
@@ -27,12 +29,47 @@ new Vue({
         this.errorMessage.name = "名前を入力してから入室してください"
         return
       }
-      this.isLogin = true
+      this.ws = new WebSocket('ws://biimata:9000')
+      this.ws.addEventListener("open", () => {
+        this.setRecieveEvent()
+        this.send({
+          type: "login",
+          name: this.name
+        })
+        this.isLogined = true
+      })
+      this.ws.addEventListener("error", () => {
+        this.errorMessage.name = "サーバーに接続できません"
+      })
     },
     logout() {
+      this.ws.send(JSON.stringify({
+        type: "logout",
+        name: this.name
+      }))
+      if (this.ws?.readyState === 1) {
+        this.ws.close()
+      }
       this.name = ""
       this.messages = []
-      this.isLogin = false
+      this.isLogined = false
+    },
+    send(data) {
+      if (this.ws?.readyState !== 1) {
+        this.logout()
+        this.errorMessage.name = "接続が切れたのでログアウトしました"
+        return
+      }
+      this.ws.send(JSON.stringify(data))
+    },
+    setRecieveEvent() {
+      this.ws.addEventListener("message", e => {
+        this.messages.push(e.data)
+        this.$nextTick().then(() => {
+          const displayMessagesEl = this.$refs.displayMessages
+          displayMessagesEl.scrollTop = displayMessagesEl.scrollHeight - displayMessagesEl.clientHeight;
+        })
+      })
     }
   }
 })
